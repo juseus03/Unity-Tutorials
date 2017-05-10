8b00ef12279fb34c0811f8854de5805a42871aad
juseus03 <juseus@gmail.com>
2017-05-09 23:23:28 -0500

    Videos 1-3

---
+++ Roll a Ball game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float speed;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Update when physics is going to happen
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+		rb.AddForce(movement*speed);
+	}
+}
