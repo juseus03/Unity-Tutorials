1e505f66fa03971fd0b2a1082a1b5f5f22052abe
juseus03 <juseus@gmail.com>
2017-05-13 01:01:29 -0500

    Finished Tutorial

--- Roll a Ball game/Assets/Scripts/PlayerController.cs
+++ Roll a Ball game/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
+    private int count;
 
 	public float speed;
+    public Text countText;
+    public Text winText;
 
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
-	}
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
 
 	// Update is called once per frame
 	void Update () {
@@ -25,4 +32,21 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
 		rb.AddForce(movement*speed);
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+            winText.text = "You Win !!!!!";
+    }
 }
