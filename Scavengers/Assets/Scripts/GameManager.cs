d7594eaf5effe6594c62b0f7091a77a15d5d33ad
juseus03 <juseus@gmail.com>
2017-07-05 17:30:59 -0500

    Completed Part 2 Video

--- Scavengers/Assets/Scripts/GameManager.cs
+++ Scavengers/Assets/Scripts/GameManager.cs
@@ -13,20 +13,20 @@ public class GameManager : MonoBehaviour
 	//Awake is always called before any Start functions
 	void Awake()
 	{
-//		//Check if instance already exists
-//		if (instance == null)
-//
-//			//if not, set instance to this
-//			instance = this;
-//
-//		//If instance already exists and it's not this:
-//		else if (instance != this)
-//
-//			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-//			Destroy(gameObject);    
-//
-//		//Sets this to not be destroyed when reloading scene
-//		DontDestroyOnLoad(gameObject);
+		//Check if instance already exists
+		if (instance == null)
+
+			//if not, set instance to this
+			instance = this;
+
+		//If instance already exists and it's not this:
+		else if (instance != this)
+
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
 
 		//Get a component reference to the attached BoardManager script
 		boardScript = GetComponent<BoardManager>();
