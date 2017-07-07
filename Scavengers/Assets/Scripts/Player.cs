f60daded54372fe4a50be4c70ebe400130ece890
juseus03 <juseus@gmail.com>
2017-07-07 00:59:38 -0500

    Add Sound Manafer, Finished

---
+++ Scavengers/Assets/Scripts/Player.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Player : MovingObject {
+
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restartLevelDelay = 1f;
+	public Text foodText;
+
+	private Animator animator;
+	private int food;
+
+	// Use this for initialization
+	protected override void Start () 
+	{
+		animator = GetComponent <Animator> ();
+		food = GameManager.instance.playerFoodPoints; //Gets food value from GameManager
+		foodText.text="Food "+food;
+		base.Start (); // Calls Start() from MovingObject
+	}
+
+	private void OnDisable () 
+	{
+		GameManager.instance.playerFoodPoints = food;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!GameManager.instance.playersTurn)
+			return;
+
+		int horizontal = 0;
+		int vertical = 0;
+
+		horizontal = (int)(Input.GetAxisRaw ("Horizontal"));
+		vertical = (int)(Input.GetAxisRaw ("Vertical"));
+
+		if (horizontal != 0) //Prevents to move diagonal
+			vertical = 0;
+
+		if (horizontal != 0 || vertical != 0)
+			AttemptMove<Wall> (horizontal, vertical);
+	}
+
+	protected override void AttemptMove<T> (int xDir, int yDir)
+	{
+		food--;
+		foodText.text="Food "+food;
+
+		base.AttemptMove<T> (xDir, yDir);
+
+		RaycastHit2D hit;
+
+		Move (xDir, yDir, out hit);
+
+		CheckIfGameOver ();
+
+		GameManager.instance.playersTurn = false;
+	}
+
+
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Exit") {
+			Invoke ("Restart", restartLevelDelay);
+			enabled = false;
+		}
+		else if (other.tag == "Food") 
+		{
+			food += pointsPerFood;
+			foodText.text="+"+pointsPerFood+" Food "+food;
+
+			other.gameObject.SetActive (false);
+		}
+		else if (other.tag == "Soda") 
+		{
+			food += pointsPerSoda;
+			foodText.text="+"+pointsPerSoda+" Food "+food;
+			other.gameObject.SetActive (false);
+		}
+	}
+
+	protected override void OnCantMove <T> (T component) 
+	{
+		Wall hitWall = component as Wall; // Cast component type to Wall
+		hitWall.DamageWall (wallDamage);
+		animator.SetTrigger ("PlayerChop");
+	}
+
+	private void Restart() 
+	{
+		SceneManager.LoadScene (0);
+	}
+
+	public void LoseFood(int loss)
+	{
+		animator.SetTrigger ("PlayerHit");
+		food -= loss;
+		foodText.text="-"+loss+" Food "+food;
+
+		CheckIfGameOver ();
+	}
+
+	private void CheckIfGameOver() 
+	{
+		if (food <= 0)
+			GameManager.instance.GameOver ();
+	}
+}
---
+++ Scavengers/Assets/Scripts/Wall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	public Sprite dmgSprite;
+	public int hp = 4;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		spriteRenderer = GetComponent <SpriteRenderer> ();
+	}
+	
+
+	public void DamageWall (int loss) 
+	{
+		spriteRenderer.sprite = dmgSprite; //Change sprite
+		hp -= loss; // substracts damage
+
+		if (hp <= 0)  //Checks if continue showing the wall
+		{
+			gameObject.SetActive (false);
+		}
+	}
+}
